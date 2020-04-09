e189775de67613a4b4ebbc9590236f593a2295a9
Léo Colombaro <git@colombaro.fr>
2020-04-10 01:57:33 +0200

    Fix styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container flex min-h-screen justify-center items-center mx-auto px-5 py-8">
+  <div class="container flex min-h-screen justify-center items-center mx-auto px-5 py-8 lg:px-64">
     <nuxt />
   </div>
 </template>
