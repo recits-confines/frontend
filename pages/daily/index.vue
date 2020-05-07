2ecaca845d9c45a3c7aa2e15f959c9ba42a39ae4
Léo Colombaro <git@colombaro.fr>
2020-05-07 17:38:16 +0200

    Import daily JSON

--- pages/daily/index.vue
+++ pages/daily/index.vue
@@ -6,14 +6,14 @@
       <FormulateInput
         name="weather"
         type="select"
-        :options="{soleil: 'Soleil', nuages: 'Nuages', pluie: 'Pluie', orages: 'Orages'}"
+        :options="{soleil: 'Soleil', nuages: 'Nuages', pluie: 'Pluie', orages:'Orages'}"
         label="Météo du jour"
         placeholder="Sélectionner"
       />
       <FormulateInput
         name="vivres"
         type="range"
-        label="Quantité de vivres"
+        label="Etat des provisions de nourriture"
         min="1"
         max="10"
         show-value="true"
@@ -35,7 +35,7 @@
       <FormulateInput
         name="sante"
         type="range"
-        label="Etat de danté physique"
+        label="Etat de santé physique"
         min="1"
         max="10"
         show-value="true"
@@ -55,91 +55,6 @@
         min="1"
         max="10"
         show-value="true"
-      />
-      <FormulateInput
-        name="opinion"
-        :options="{
-          self_health: 'Ma santé personnelle',
-          other_health: 'La santé de mes proches',
-          financial: 'Mes conditions financières',
-          criris: 'La situation sanitaire globale'
-        }"
-        type="checkbox"
-        label="Je m’inquiète pour..."
-      />
-      <FormulateInput
-        name="alcohol"
-        :options="{more: '+ (en augmentation)', stable: '= (stable)', less: '- (en diminution)', none: '0 (aucune)'}"
-        type="radio"
-        label="Consommation d’alcool"
-      />
-      <FormulateInput
-        name="tabaco"
-        :options="{more: '+ (en augmentation)', stable: '= (stable)', less: '- (en diminution)', none: '0 (aucune)'}"
-        type="radio"
-        label="Consommation de tabac"
-      />
-      <FormulateInput
-        name="sleep"
-        :options="{more: 'Bon', stable: 'Moyen', less: 'Mauvais'}"
-        type="radio"
-        label="Etat du sommeil"
-      />
-      <FormulateInput
-        v-model="sorties"
-        name="sorties"
-        type="number"
-        label="Nombres de sorties"
-        min="0"
-      />
-      <FormulateInput
-        v-if="sorties > 0"
-        name="sorties_goal"
-        :options="{
-          sport: 'faire du sport',
-          food: 'achat de nourriture',
-          pets: 'promener le chien',
-          work: 'le travail'
-        }"
-        type="checkbox"
-        label="Sorties pour..."
-      />
-      <FormulateInput
-        name="delivery"
-        :options="{true: 'Oui', false: 'Non'}"
-        type="radio"
-        label="Livraison de nourriture à domicile"
-      />
-      <FormulateInput
-        name="routine"
-        :options="{true: 'Oui', false: 'Non'}"
-        type="radio"
-        label="Routine quotidienne (douche, habillement…)"
-      />
-      <FormulateInput
-        name="work"
-        type="number"
-        label="Nombres d'heures de travail"
-        min="0"
-        max="24"
-      />
-      <FormulateInput
-        name="school"
-        :options="{true: 'Oui', false: 'Non'}"
-        type="radio"
-        label="Ecole / devoirs des enfants"
-      />
-      <FormulateInput
-        name="extra_cleanup"
-        :options="{true: 'Oui', false: 'Non'}"
-        type="radio"
-        label="Etat du sommeil"
-      />
-      <FormulateInput
-        name="_gotcha"
-        type="text"
-        label="Vous avez fait du rien ? Ce champs doit rester vide"
-        style="display:none"
       />
 
       <FormulateErrors />
--- pages/daily/two.vue
+++ pages/daily/two.vue
@@ -7,6 +7,7 @@
 
 <script>
 import Swipe from '@/components/Swipe'
+import stack from '@/static/stacks/daily.json'
 
 export default {
   components: {
@@ -14,107 +15,7 @@ export default {
   },
   data () {
     return {
-      cards: [
-        {
-          name: 'training',
-          context: 'Pour répondre',
-          description: 'Oui en glissant à droite. Non à gauche.',
-          icon: '↔️'
-        },
-        {
-          name: 'inquiet',
-          description: 'Je suis inquiet',
-          icon: '🤞🏼'
-        },
-        {
-          name: 'inquietProches',
-          description: 'Pour la santé de mes proches',
-          icon: '👨‍👩‍👧‍👦',
-          relates: 'inquiet'
-        },
-        {
-          name: 'inquietFinances',
-          description: 'Pour mes conditions financières',
-          icon: '💵',
-          relates: 'inquiet'
-        },
-        {
-          name: 'inquietSanitaire',
-          description: 'Pour la situation sanitaire globale',
-          icon: '😷',
-          relates: 'inquiet'
-        },
-        {
-          name: 'alcool',
-          description: 'J’ai consommé de l’alcool',
-          icon: '🍻'
-        },
-        {
-          name: 'sommeil',
-          description: 'Cette nuit, j’ai bien dormi',
-          icon: '🛏'
-        },
-        {
-          name: 'sortie',
-          description: 'Je suis sorti',
-          icon: '🌳'
-        },
-        {
-          name: 'sortieSport',
-          description: 'Pour faire du sport',
-          icon: '🏃',
-          relates: 'sortie'
-        },
-        {
-          name: 'sortieCourses',
-          description: 'Pour acheter des provision',
-          icon: '🛒',
-          relates: 'sortie'
-        },
-        {
-          name: 'sortieChien',
-          description: 'Pour promener le chien',
-          icon: '🐶',
-          relates: 'sortie'
-        },
-        {
-          name: 'sortieTravail',
-          description: 'Pour aller travailler',
-          icon: '🚗',
-          relates: 'sortie'
-        },
-        {
-          name: 'sortieAutre',
-          description: 'Pour une autre raison',
-          icon: '🏙',
-          relates: 'sortie'
-        },
-        {
-          name: 'livraison',
-          description: 'J’ai été livré à domicile',
-          icon: '📦'
-        },
-        {
-          name: 'routineDouche',
-          description: 'Je me suis douché/habillé',
-          icon: '🚿'
-        },
-        {
-          name: 'devoir',
-          description: 'J’ai aidé aux devoirs des enfants',
-          icon: '📚'
-        },
-        {
-          name: 'menageBrico',
-          description: 'J’ai bricolé ou fais le ménage',
-          icon: '🧽'
-        },
-        {
-          name: 'sportDomicile',
-          description: 'J’ai fait du sport à la maison',
-          icon: '🤸'
-        }
-      ].reverse()
+      cards: stack.reverse()
     }
   },
   async beforeMount () {
