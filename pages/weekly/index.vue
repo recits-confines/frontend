1081fd542d3938bebf6f6497d35fd860b873e04d
Léo Colombaro <git@colombaro.fr>
2020-05-17 18:37:01 +0200

    Improve style

--- pages/weekly/index.vue
+++ pages/weekly/index.vue
@@ -4,16 +4,18 @@
       v-for="(form, index) in forms"
       :key="form.title"
       :to="`/weekly/${index}`"
-      class="bg-main hover:bg-transparent uppercase font-black text-background hover:text-main py-5 px-6 my-4 border border-transparent hover:border-main rounded-full"
+      class="flex items-center justify-between bg-secondary hover:bg-transparent uppercase font-black text-background hover:text-secondary py-5 px-6 my-4 border border-transparent hover:border-secondary rounded-full"
     >
       {{ form.title }}
+      <svg class="fill-current h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
     </nuxt-link>
-    <FormulateInput
+    <button
       v-if="dataCount"
-      label="Je soumets mon rapport hebdomadaire"
-      type="submit"
-      @click.prevent="submit"
-    />
+      class="bg-main hover:bg-transparent uppercase font-black text-background hover:text-main py-5 px-6 my-4 border border-transparent hover:border-main rounded-full"
+      @click="submit"
+    >
+      Je soumets mon rapport hebdomadaire
+    </button>
   </Page>
 </template>
 
